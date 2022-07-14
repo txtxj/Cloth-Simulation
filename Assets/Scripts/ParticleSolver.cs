6e52b573f711617f2209df4e43dbe73ada170671
txtxj <1418373194@qq.com>
2022-07-15 02:01:53 +0800

    feat: finish Explicit Euler Cloth Simulation

--- Assets/Scripts/ParticleSolver.cs
+++ Assets/Scripts/ParticleSolver.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
 public class ParticleSolver : MonoBehaviour
@@ -11,8 +12,9 @@ public class ParticleSolver : MonoBehaviour
     
     public ComputeShader compute;
     public Material material;
-    public int particleCount;
-    
+    public Vector2Int clothSize;
+    public float gapSize;
+
     public float dt;
     public float ks;
     public float kd;
@@ -22,9 +24,10 @@ public class ParticleSolver : MonoBehaviour
     private int particleKernelId;
     private int springKernelId;
     private ComputeBuffer m_ParticleBuffer;
-    private ComputeBuffer m_SpingBuffer;
+    private ComputeBuffer m_SpringBuffer;
     private int particleWarpCount;
     private int springWarpCount;
+    private int particleCount;
     private int springCount;
 
     private struct Particle
@@ -39,44 +42,79 @@ public class ParticleSolver : MonoBehaviour
     {
         public Vector2Int node;
         public float length;
+        public Spring(int a, int b, Particle[] particles)
+        {
+            node = new Vector2Int(a, b);
+            length = (particles[a].position - particles[b].position).magnitude;
+        }
     }
 
     private void InitiateParticles(Particle[] particles)
     {
-        for (int i = 0; i < particleCount; i++)
+        for (int i = 0; i < clothSize.x; i++)
         {
-            particles[i].position = new Vector3(i, 0f, 0f);
-            particles[i].velocity = Vector3.zero;
-            particles[i].force = Vector3.zero;
-            particles[i].isFixed = i == 0 ? 1f : 0f;
+            for (int j = 0; j < clothSize.y; j++)
+            {
+                particles[i * clothSize.y + j].position = new Vector3(-i, 0f, j) * gapSize;
+                particles[i * clothSize.y + j].velocity = Vector3.zero;
+                particles[i * clothSize.y + j].force = Vector3.zero;
+                particles[i * clothSize.y + j].isFixed = j == 0 ? 1f : 0f;
+            }
         }
     }
 
-    private void InitiateSprings(Spring[] springs, Particle[] particles)
+    private void InitiateSprings(List<Spring> springs, Particle[] particles)
     {
-        for (int i = 0; i < springCount; i++)
+        for (int i = 0; i < clothSize.x; i++)
         {
-            springs[i].node = new Vector2Int(i, i + 1);
-            springs[i].length = (particles[springs[i].node.x].position - particles[springs[i].node.y].position).magnitude;
+            for (int j = 0; j < clothSize.y; j++)
+            {
+                int u = i * clothSize.y + j;
+                if (j != clothSize.y - 1)
+                {
+                    springs.Add(new Spring(u, u + 1, particles));
+                }
+                if (i != clothSize.x - 1)
+                {
+                    springs.Add(new Spring(u, u + clothSize.y, particles));
+                }
+                if (j != clothSize.y - 1 && j != clothSize.y - 2)
+                {
+                    springs.Add(new Spring(u, u + 2, particles));
+                }
+                if (i != clothSize.x - 1 && i != clothSize.x - 2)
+                {
+                    springs.Add(new Spring(u, u + clothSize.y * 2, particles));
+                }
+                if (j != clothSize.y - 1 && i != clothSize.x - 1)
+                {
+                    springs.Add(new Spring(u, u + clothSize.y + 1, particles));
+                }
+                if (j != clothSize.y - 1 && i != 0)
+                {
+                    springs.Add(new Spring(u, u - clothSize.y + 1, particles));
+                }
+            }
         }
     }
 
     private void Start()
     {
-        springCount = particleCount - 1;
-        m_ParticleBuffer = new ComputeBuffer(particleCount, Marshal.SizeOf(typeof(Particle)));
-        m_SpingBuffer = new ComputeBuffer(springCount, Marshal.SizeOf(typeof(Spring)));
+        particleCount = clothSize.x * clothSize.y;
         Particle[] particles = new Particle[particleCount];
-        Spring[] springs = new Spring[springCount];
+        List<Spring> springs = new List<Spring>();
         InitiateParticles(particles);
         InitiateSprings(springs, particles);
+        springCount = springs.Count;
+        m_ParticleBuffer = new ComputeBuffer(particleCount, Marshal.SizeOf(typeof(Particle)));
+        m_SpringBuffer = new ComputeBuffer(springCount, Marshal.SizeOf(typeof(Spring)));
         m_ParticleBuffer.SetData(particles);
-        m_SpingBuffer.SetData(springs);
+        m_SpringBuffer.SetData(springs);
         particleKernelId = compute.FindKernel("UpdateParticles");
         springKernelId = compute.FindKernel("UpdateSprings");
         compute.SetBuffer(particleKernelId, "particles", m_ParticleBuffer);
         compute.SetBuffer(springKernelId, "particles", m_ParticleBuffer);
-        compute.SetBuffer(springKernelId, "springs", m_SpingBuffer);
+        compute.SetBuffer(springKernelId, "springs", m_SpringBuffer);
         compute.SetFloat("dt", dt);
         compute.SetFloat("ks", ks);
         compute.SetFloat("kd", kd);
---
+++ README.md
@@ -0,0 +1,5 @@
+## 参考资料
+ - https://zhuanlan.zhihu.com/p/33675797
+ - https://github.com/inact1v1ty/Compute-shader-particles
+ - https://www.cnblogs.com/shushen/p/5473264.html
+ - https://www.bilibili.com/video/BV1X7411F744?p=22
