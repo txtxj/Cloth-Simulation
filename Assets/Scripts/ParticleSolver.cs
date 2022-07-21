0e086026c240a7489152603401412320ad06edf9
txtxj <1418373194@qq.com>
2022-07-21 15:55:22 +0800

    refactor: delete unnecessary using

--- Assets/Scripts/ParticleSolver.cs
+++ Assets/Scripts/ParticleSolver.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UIElements;
 using Vector3 = UnityEngine.Vector3;
 
 public class ParticleSolver : MonoBehaviour
